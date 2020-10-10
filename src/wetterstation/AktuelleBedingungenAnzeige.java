8c7dd3c1ab29bfc327fa165ebbd1c0a504d410a3
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2020-10-10 15:57:59 +0200

    Add current conditions display

---
+++ src/wetterstation/AktuelleBedingungenAnzeige.java
@@ -0,0 +1,17 @@
+package wetterstation;
+	
+public class AktuelleBedingungenAnzeige {
+	private float temperatur;
+	private float feuchtigkeit;
+	
+	public void aktualisieren(float temp, float feucht, float druck) {
+		this.temperatur = temp;
+		this.feuchtigkeit = feucht;
+		anzeigen();
+	}
+	
+	public void anzeigen() {
+		System.out.println("Aktuelle Bedingungen: " + temperatur 
+			+ " Grad C und " + feuchtigkeit + "% Luftfeuchtigkeit");
+	}
+}
