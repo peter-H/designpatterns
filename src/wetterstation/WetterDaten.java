349ea4e9f28e3c36a6f0f4bee6ccbd64a04f47ae
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2020-10-10 15:53:43 +0200

    Add weather data and station

---
+++ src/wetterstation/WetterDaten.java
@@ -0,0 +1,41 @@
+package wetterstation;
+
+public class WetterDaten {
+	private float temperatur;
+	private float feuchtigkeit;
+	private float luftdruck;
+	
+	public void messwerteGeaendert() {
+		System.out.println(this.toString());
+	}
+	
+	public void setMesswerte(float temp, float feucht, float druck) {
+		this.temperatur = temp;
+		this.feuchtigkeit = feucht;
+		this.luftdruck = druck;
+		messwerteGeaendert();
+	}
+	
+	public float getTemperatur() {
+		return temperatur;
+	}
+	
+	public float getFeuchtigkeit() {
+		return feuchtigkeit;
+	}
+	
+	public float getLuftdruck() {
+		return luftdruck;
+	}
+	
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append("Messwerte:\n");
+		result.append("----------\n");
+		result.append("Temperatur: " + getTemperatur() + "\n");
+		result.append("Luftfeuchtigkeit: " + getFeuchtigkeit() + "\n");
+		result.append("Luftdruck: " + getLuftdruck() + "\n");
+		result.append("\n");
+		return result.toString();
+	}
+}
---
+++ src/wetterstation/WetterStation.java
@@ -0,0 +1,12 @@
+package wetterstation;
+
+public class WetterStation {
+
+	public static void main(String[] args) {
+		WetterDaten wetterDaten = new WetterDaten();
+	
+		wetterDaten.setMesswerte(30, 65, 30.4f);
+		wetterDaten.setMesswerte(32, 70, 29.2f);
+		wetterDaten.setMesswerte(28, 90, 29.2f);
+	}
+}
