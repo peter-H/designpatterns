f6da0d96fa7dc86931f1440b6620ed0568cae6da
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2020-10-10 15:47:11 +0200

    Interface injection instead of constructor injection

--- src/pizzeria/PizzaFranchise.java
+++ src/pizzeria/PizzaFranchise.java
@@ -3,13 +3,9 @@ package pizzeria;
 import com.google.inject.Inject;
 
 public class PizzaFranchise {
-	private PizzaStore store;
-	
 	@Inject
-	public PizzaFranchise(PizzaStore store) {
-		this.store = store;
-	}
-	
+	private PizzaStore store;
+
 	public Pizza orderPizza(String type) {
 		return store.orderPizza(type);
 	}
